14f9e2fb5293125da429393b6cb34b7505430e58
FlyingSpell <50572171+FlyingSpell@users.noreply.github.com>
2021-05-20 19:15:08 +0300

    Added main menu

---
+++ Controllers/OrderListController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrderWebApp.Controllers
+{
+    public class OrderListController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
