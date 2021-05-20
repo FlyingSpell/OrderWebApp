ba3a8ace658e1efb80320debac123d366b92e01f
FlyingSpell <50572171+FlyingSpell@users.noreply.github.com>
2021-05-20 22:20:22 +0300

    Added order list page (just view - no data)

--- Controllers/OrderListController.cs
+++ Controllers/OrderListController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using OrderWebApp.Models;
+using System.Collections.Generic;
 
 namespace OrderWebApp.Controllers
 {
     public class OrderListController : Controller
     {
+        private List<Order> m_OrderList;
+        public OrderListController()
+        {
+            m_OrderList = new List<Order>()
+            {
+                new Order()
+                {
+                    SenderAddress = "asdasd",
+                    SenderCity = "dfsdfsfd",
+                    RecipientAddress = "dfgdfgdfgdfg",
+                    RecipientCity = "dfgdfdfgdfg",
+                    PackageWeightInkilograms = 10,
+                    PackagePickUpDate = DateTime.Now
+                }
+            };
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(m_OrderList);
         }
     }
 }
---
+++ Models/OrderModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OrderWebApp.Models
+{
+    public class Order
+    {
+        public String   SenderCity { get; set; }
+        public String   SenderAddress { get; set; }
+        public String   RecipientCity { get; set; }
+        public String   RecipientAddress { get; set; }
+        public int      PackageWeightInkilograms { get; set; }
+        public DateTime PackagePickUpDate { get; set; }
+
+    }
+}
